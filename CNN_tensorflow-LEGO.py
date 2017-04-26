04a940157d2d49e673049518357494810ff2ed69
Petr Kaspar <petr1kaspar@gmail.com>
2017-04-26 22:48:07 +0200

    Adding progress bar and images read from array of paths

--- CNN_tensorflow-LEGO.py
+++ CNN_tensorflow-LEGO.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import time
 from tensorflow.examples.tutorials.mnist import input_data
 import struct
 import numpy as np
@@ -7,20 +8,56 @@ from os import listdir
 import os
 from os.path import isfile, join
 from scipy import misc
+from sys import stdout
 # import Image
 from PIL import Image
 # import cv2
 
 # --------------------------------------------------------------------
 imgDirectoryPath = 'images/lego/'
-imgTrainingDirectory = 'images/lego/training4/'
-imgTestDirectory = 'images/lego/test4/'
-subDirectory = '/diff/'
+# imgTrainingDirectory = 'images/lego/training4/'
+# imgTestDirectory = 'images/lego/test4/'
+# subDirectory = '/diff/'
+subDirectory = ''
 filename = 'finalized_model.pkl'
 
+imgTrainingDirectory = 'images/lego2/train2/'
+imgTestDirectory = 'images/lego2/test2/'
+
 n_classes = 0
-nSlozek = 2
+nSlozek = 4
 q = [0] * nSlozek
+
+def getAllImageNamesFromFolder(image_folders):
+    # Vrati pole
+
+    X = []
+    y = []
+
+    for index, folder in enumerate(os.scandir(image_folders)):
+        print(folder.name)
+        pid = image_folders + folder.name   # pid = path_to_image_directory
+        print(pid)
+        x_subFolder = []
+        iii = 0
+        [x_subFolder.append(pid+'/'+file) for file in listdir(pid) if isfile(join(pid, file))]
+        print('nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn')
+        print(x_subFolder)
+        print('------------------------------------------------------------------------')
+        # X.append(x_subFolder)
+        X = np.concatenate((X, x_subFolder))
+        print(X)
+        print('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')
+        for ii in range(len(X)):
+            q[index] = 1
+            y.append(q.copy())
+            q[index] = 0
+        # y = y + [os.path.basename(folder.name)] * len(x_subFolder)
+        print(y)
+    print(X)
+    print(X[0])
+    return X, y
+
 def getSVMinputData(trainingDir):
     print('======================  getSVMinputData(trainingDir) ====================')
     X = np.array([[]])
@@ -63,6 +100,7 @@ def getMultiple_HOG(folder):
         log = [str(n+1), '/', str(len(onlyfiles)),' ']
         print(''.join(log), join(os.path.basename(folder), onlyfiles[n]))
         im = misc.imread(join(folder, onlyfiles[n]))
+        # im = misc.imread(join(folder, onlyfiles[n]), mode='L') # L = 8-bit gray
         # im = Image.open(join(folder, onlyfiles[n]))
         # im = cv2.cvtColor(cv2.imread(join(folder, onlyfiles[n])), cv2.COLOR_BGR2GRAY)
         X.append(im)
@@ -97,7 +135,27 @@ def getMultiple_HOG(folder):
 
     return nImages, X
 
-
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+    """
+    filled = '█'
+    unfilled = '░'
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = filled * filledLength + unfilled * (length - filledLength)
+    # print("\r%s |%s| %s%% %s" % (prefix, bar, percent, suffix), end = "\r")
+    stdout.write('\x1b[2K\r%s |%s| %s%s  %s/%s %s' % (prefix, bar, percent, '%', iteration, total, suffix))
+    # Print New Line on Complete
+    # if iteration == total:
+    #     print()
 # *********************************************************************
 img_index = 59999
 minst_imgs_path = 'MNIST_data/t10k-images.idx3-ubyte'
@@ -162,12 +220,12 @@ n_nodes_hl3 = 500
 n_classes = nSlozek
 batch_size = 100
 
-x = tf.placeholder('float', [None, 320*240])
+x = tf.placeholder('float', [None, 400*300*3])
 y = tf.placeholder('float')
 
 
 def neural_network_model(data):
-    hidden_1_layer = {'weights': tf.Variable(tf.random_normal([320*240, n_nodes_hl1])),
+    hidden_1_layer = {'weights': tf.Variable(tf.random_normal([400*300*3, n_nodes_hl1])),
                       'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
     hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
@@ -210,19 +268,26 @@ def convolutional_neural_network(x):
         'W_conv1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
         # 5x5 conv, 32 inputs, 64 outputs
         'W_conv2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+
+        # 'W_conv3': tf.Variable(tf.random_normal([5, 5, 64, 128])),
+
+        # 'W_conv4': tf.Variable(tf.random_normal([5, 5, 128, 256])),
+
         # fully connected, 7*7*64 inputs, 1024 outputs
-        'W_fc': tf.Variable(tf.random_normal([4 * 240*320, 1024])),
+        'W_fc': tf.Variable(tf.random_normal([ 4*400*300*3, 64])),
         # 1024 inputs, 10 outputs (class prediction)
-        'out': tf.Variable(tf.random_normal([1024, n_classes]))
+        'out': tf.Variable(tf.random_normal([64, n_classes]))
     }
 
     biases = {'b_conv1': tf.Variable(tf.random_normal([32])),
               'b_conv2': tf.Variable(tf.random_normal([64])),
-              'b_fc': tf.Variable(tf.random_normal([1024])),
+              # 'b_conv3': tf.Variable(tf.random_normal([128])),
+              # 'b_conv4': tf.Variable(tf.random_normal([256])),
+              'b_fc': tf.Variable(tf.random_normal([64])),
               'out': tf.Variable(tf.random_normal([n_classes]))}
 
     # Reshape input to a 4D tensor
-    x = tf.reshape(x, shape=[-1, 240, 320, 1])
+    x = tf.reshape(x, shape=[-1, 300, 400, 1])
     # Convolution Layer, using our function
     conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
     # Max Pooling (down-sampling)
@@ -232,9 +297,16 @@ def convolutional_neural_network(x):
     # Max Pooling (down-sampling)
     conv2 = maxpool2d(conv2)
 
+    # conv3 = tf.nn.relu(conv2d(conv2, weights['W_conv3']) + biases['b_conv3'])
+    # # Max Pooling (down-sampling)
+    # conv3 = maxpool2d(conv3)
+
+    # conv4 = tf.nn.relu(conv2d(conv3, weights['W_conv4']) + biases['b_conv4'])
+    # # Max Pooling (down-sampling)
+    # conv4 = maxpool2d(conv4)
     # Fully connected layer
     # Reshape conv2 output to fit fully connected layer
-    fc = tf.reshape(conv2, [-1, 4 * 240*320])
+    fc = tf.reshape(conv2, [-1, 4 * 300*400*3])
     fc = tf.nn.relu(tf.matmul(fc, weights['W_fc']) + biases['b_fc'])
     fc = tf.nn.dropout(fc, keep_rate)
 
@@ -245,7 +317,7 @@ def convolutional_neural_network(x):
 
 def train_neural_network(x, XX, yy):
 
-    prediction = neural_network_model(x)
+    prediction = convolutional_neural_network(x)
     # prediction = convolutional_neural_network(x)
 
     # OLD VERSION:
@@ -257,7 +329,7 @@ def train_neural_network(x, XX, yy):
                                                          # with hm_epochs = 10; batch_size = 10 => Accuracy: 0.9684
 
     # hm_epochs = 1       #hm_epochs = 1 => Accuracy: 0.9194
-    hm_epochs = 2
+    hm_epochs = 15
 
     # model_saver = tf.train.Saver()
 
@@ -268,6 +340,7 @@ def train_neural_network(x, XX, yy):
         sess.run(tf.global_variables_initializer())
 
         for epoch in range(hm_epochs):
+            start_time = time.time()
             epoch_loss = 0
             # for _ in range(int(mnist.train.num_examples / batch_size)):
             for ii in range(XX.shape[0]):
@@ -278,8 +351,10 @@ def train_neural_network(x, XX, yy):
                 # print(epoch_y)
                 _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
-
-            print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+            # todo: mereni casu pro kazdou epochu
+            end_time = time.time()
+            # print("total epoch time: ", round(end_time - start_time), "s")
+            print('Epoch', epoch+1, 'completed out of', hm_epochs, 'for time', round(end_time - start_time), 's, loss:', epoch_loss)
 
 
         # construct model
@@ -315,11 +390,11 @@ def train_neural_network(x, XX, yy):
 
             # ....................................................
             x_in = [np.array(X_test[iii].flatten(), dtype=float)]
-            print(y_test[iii], ' => digit: ', np.argmax(y_test[iii]))
+            # print(y_test[iii], ' => digit: ', np.argmax(y_test[iii]))
             classification = sess.run(tf.argmax(prediction, 1), feed_dict={x: x_in})
 
-            print("correct digit = ", np.argmax(y_test[iii]), ";         classification = ", classification[0])
-            print('Neural Network predicted', classification, "for your digit")
+            # print("correct digit = ", np.argmax(y_test[iii]), ";         classification = ", classification[0])
+            print('Neural Network predicted', classification, "for LEGO piece", np.argmax(y_test[iii]))
 
         print('.....................................................')
         # X_test = [np.array(X_test[0].flatten(), dtype=float)]
@@ -432,16 +507,90 @@ def train_neural_network(x, XX, yy):
         # img_plot = plt.imshow(image, 'Greys')  # ; plt.title('Label = ' + str(label))
         print('mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm')
 
+def train_neural_network2(x, XX, yy):
+
+    prediction = convolutional_neural_network(x)
+    # prediction = convolutional_neural_network(x)
+
+    # OLD VERSION:
+    # cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
+    # NEW:
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)  # with hm_epochs = 10 => Accuracy: 0.9523
+    # optimizer = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)    # with hm_epochs = 10 => Accuracy: 0.9541
+    # with hm_epochs = 10; batch_size = 10 => Accuracy: 0.9684
+
+    # hm_epochs = 1       #hm_epochs = 1 => Accuracy: 0.9194
+    hm_epochs = 2
+
+    # model_saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        # OLD:
+        # sess.run(tf.initialize_all_variables())
+        # NEW:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(hm_epochs):
+            start_time = time.time()
+            epoch_loss = 0
+            # for _ in range(int(mnist.train.num_examples / batch_size)):
+            for ii in range(XX.shape[0]):
+                printProgressBar(ii, len(XX), prefix='Progress:', suffix='Complete', length=50)
+
+                # epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+
+                image = misc.imread(XX[ii])
+
+                epoch_x = [np.array(image.flatten(), dtype=float)]
+                epoch_y = yy[ii]
+                # print(epoch_x)
+                # print(epoch_y)
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_loss += c
+            # todo: mereni casu pro kazdou epochu
+            end_time = time.time()
+            # print("total epoch time: ", round(end_time - start_time), "s")
+            # \r vymaze(prepise) predesli radek v konzoli, v tomto pripade ProgressBar a nahradiho informacnim vypisem
+            print('\rEpoch', epoch + 1, 'completed out of', hm_epochs, 'for time', round(end_time - start_time),
+                  's, loss:', epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+
+        X_test, y_test = getSVMinputData(imgTestDirectory)
+        # y_test = [['1','0'],['1','0'],['1','0'],['0','1'],['0','1'],['0','1']]
+
+        XXX = []
+        print('.....................................................')
+        for iii in range(X_test.shape[0]):
+            XXX.append(np.array(X_test[iii].flatten(), dtype=float))
+
+            # ....................................................
+            x_in = [np.array(X_test[iii].flatten(), dtype=float)]
+            # print(y_test[iii], ' => digit: ', np.argmax(y_test[iii]))
+            classification = sess.run(tf.argmax(prediction, 1), feed_dict={x: x_in})
+
+            # print("correct digit = ", np.argmax(y_test[iii]), ";         classification = ", classification[0])
+            print('Neural Network predicted', classification, "for LEGO piece", np.argmax(y_test[iii]))
+
+        print('.....................................................')
+        # X_test = [np.array(X_test[0].flatten(), dtype=float)]
+        print('Accuracy:', accuracy.eval({x: XXX, y: y_test}))
+        print('mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm')
 
 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# XX, yy = getSVMinputData(imgTrainingDirectory)
+# print('yy ', yy)  # yy = [['1', '0'], ['1', '0'], ['1', '0'], ['0', '1'], ['0', '1'], ['0', '1']]
 
-XX, yy = getSVMinputData(imgTrainingDirectory)
+# train_neural_network(x, XX, yy) #   , XX.shape[0]
+# //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+XX, yy = getAllImageNamesFromFolder(imgTrainingDirectory)
 print('yy ', yy)
-
-
-# yy = [['1', '0'], ['1', '0'], ['1', '0'], ['0', '1'], ['0', '1'], ['0', '1']]
-
-train_neural_network(x, XX, yy) #   , XX.shape[0]
+train_neural_network2(x, XX, yy) #   , XX.shape[0]
 
 """
 X, y = getSVMinputData(imgTrainingDirectory)
